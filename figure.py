a5142fc00668de371623fffbbc08c35e704fad1c
jackbenn <jack@bennetto.com>
2019-04-24 17:28:08 -0700

    add transformation with model and scoring against target

--- figure.py
+++ figure.py
@@ -3,8 +3,8 @@ from PIL import Image, ImageDraw
 
 class Figure:
     n_genes = 50
-    mutation_rate = 0.02
-    mutation_prop = 0.01
+    mutation_rate = 0.1
+    mutation_prop = 0.1
     size = 224
 
     def __init__(self, genes=None):
@@ -22,7 +22,7 @@ class Figure:
             self.genes = genes
 
     def clone_and_mutate(self):
-        clone = Figure(self.genes)
+        clone = Figure(self.genes.copy())
         clone.genes += (np.random.randint(0, 2, (self.n_genes, 7)) *
                         np.random.randn(self.n_genes, 7) * self.mutation_rate)
         return clone
@@ -34,19 +34,29 @@ class Figure:
                              other.genes)
         return Figure(new_genes)
     
-    def draw(self, size):
+    def draw(self):
         im = Image.new('RGB', (self.size, self.size))
         draw = ImageDraw.Draw(im)
         sorted_genes = self.genes[np.argsort(self.genes[:, 6])]
-        base_coords = size * np.array([[-1, -1], [1, 1]])
+        base_coords = self.size * np.array([[-1, -1], [1, 1]])
         for gene in sorted_genes:
             coords = gene[2] * base_coords
-            coords = (coords + size * gene[[0,1]]).flatten().tolist()
+            coords = (coords + self.size * gene[[0,1]]).flatten().tolist()
             color = tuple((gene[[3,4,5]] * 255).astype(int))
             #print(color)
             #print(coords)
             #print()              
             draw.ellipse(coords, color)
         return im
-                        
+
+    def transform(self, model):
+        im = self.draw()
+        return model.predict(np.array(im)[None, ...]).flatten()
+    
+    def score(self, model, target):
+        '''
+        Assume target is normalized already
+        '''
+        transformed = self.transform(model)
+        return transformed @ target / np.linalg.norm(transformed) 
             
